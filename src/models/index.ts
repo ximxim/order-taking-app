52dc45c72c927759d6ecabb6a4cf04e58d45a100
Azim Ahmed <azim.ahmed7@gmail.com>
2023-05-09 10:34:10 -0400

    typescript models

---
+++ src/models/index.ts
@@ -0,0 +1,76 @@
+export type PaymentMethodType = 'cash' | 'card' | 'phone';
+
+export interface IRestraunt {
+	name: string;
+	address: string;
+	phone: string;
+	closingTime: string;
+	openingTime: string;
+	paymentMethods: PaymentMethodType[];
+}
+
+export interface ICategory {
+	id: string;
+	createdate: string;
+	lastupdate: string;
+	createby: string;
+	image: string;
+	title: string;
+	description: string;
+}
+
+export interface IItem {
+	id: string;
+	createdate: string;
+	lastupdate: string;
+	createby: string;
+	image: string;
+	description: string;
+	price: number;
+	label: string;
+	category: string;
+	variants: IVariant[];
+}
+
+export interface IVariant {
+	isRequired: boolean;
+	allowMultiple: boolean;
+	type: string;
+	choices: IChoice[];
+}
+
+export interface IChoice {
+	price: number;
+	label: string;
+}
+
+export type OrderStatus = 'pending' | 'confirmed' | 'cancelled' | 'completed';
+
+export interface IOrder {
+	firstName: string;
+	lastName: string;
+	email: string;
+	phone: string;
+	lines: ILine[];
+	comments?: string;
+	reason?: string;
+	pickupTime: string;
+	paymentMethod: PaymentMethodType[];
+	status?: OrderStatus[];
+	subTotal: number;
+	total: number;
+}
+
+export interface ILine {
+	label: string;
+	price: number;
+	quantity: number;
+	instructions: string;
+	value: ILineValue[];
+}
+
+export interface ILineValue {
+	variant: string;
+	value: string;
+	price: number;
+}
