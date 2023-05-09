810390b014d3028533307e83f6f69ebd835eda69
Azim Ahmed <azim.ahmed7@gmail.com>
2023-05-09 12:01:32 -0400

    Setup react-admin-firebase data provider

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -1,5 +1,23 @@
-import { Admin as RAdmin } from "react-admin";
+import { Admin as RAdmin, Resource, ListGuesser } from "react-admin";
+import { RAFirebaseOptions, FirebaseDataProvider } from "react-admin-firebase";
+
+import { firebaseConfig } from "../utils/firebase";
+
+const options: RAFirebaseOptions = {
+  logging: true,
+  persistence: "session",
+  lazyLoading: {
+    enabled: true,
+  },
+  watch: ["orders"],
+};
+
+const dataProvider = FirebaseDataProvider(firebaseConfig, options);
 
 export const Admin = () => {
-  return <RAdmin>{/*  */}</RAdmin>;
+  return (
+    <RAdmin basename="/admin" dataProvider={dataProvider}>
+      <Resource name="items" list={ListGuesser} />
+    </RAdmin>
+  );
 };
