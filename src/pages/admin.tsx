674964aacaa06553159bbd9f92bb4b117de0f086
Azim Ahmed <azim.ahmed7@gmail.com>
2023-05-10 16:50:17 -0400

    Setup navigation to restaurant form

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -3,12 +3,17 @@ import {
   Resource,
   ListGuesser,
   CustomRoutes,
+  Layout,
+  AppBar,
+  RefreshIconButton,
 } from "react-admin";
 import {
   RAFirebaseOptions,
   FirebaseDataProvider,
   FirebaseAuthProvider,
 } from "react-admin-firebase";
+import { IconButton } from "@mui/material";
+import SettingsIcon from "@mui/icons-material/Settings";
 
 import { CategoryProps } from "../components/category-resource";
 import { firebaseConfig } from "../utils/firebase";
@@ -28,9 +33,25 @@ const options: RAFirebaseOptions = {
 const dataProvider = FirebaseDataProvider(firebaseConfig, options);
 const authProvider = FirebaseAuthProvider(firebaseConfig, {});
 
+const MyAppBar = () => (
+  <AppBar
+    toolbar={
+      <>
+        <RefreshIconButton />
+        <IconButton href="/admin/info">
+          <SettingsIcon style={{ color: "white" }} />
+        </IconButton>
+      </>
+    }
+  />
+);
+
+export const MyLayout = (props: any) => <Layout {...props} appBar={MyAppBar} />;
+
 export const Admin = () => {
   return (
     <RAdmin
+      layout={MyLayout}
       authProvider={authProvider}
       basename="/admin"
       dataProvider={dataProvider}
