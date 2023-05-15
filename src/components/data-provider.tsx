254fbf1d6ccac84d4f64d1e544d6e310494f363b
Azim Ahmed <azim.ahmed7@gmail.com>
2023-05-15 10:50:05 -0400

    Fetch restaurant info

--- src/components/data-provider.tsx
+++ src/components/data-provider.tsx
@@ -3,19 +3,44 @@ import {
   FunctionComponent,
   PropsWithChildren,
   createContext,
+  useContext,
+  useEffect,
   useState,
 } from "react";
+import { IRestraunt } from "../models";
+import { doc, getDoc, getDocs } from "firebase/firestore";
+import { db } from "../utils/firebase";
 
-interface IDataProviderContext {}
+interface IDataProviderContext {
+  restaurantInfo?: IRestraunt;
+}
 
 const DataProviderContext = createContext<IDataProviderContext>({});
+
+export const useDataProvider = () => useContext(DataProviderContext);
 
 export const DataProvider: FunctionComponent<PropsWithChildren> = ({
   children,
 }) => {
   const [isReady, setIsReady] = useState(false);
+  const [restaurantInfo, setRestaurantInfo] = useState<IRestraunt>();
+
+  const fetchRestaurantInfo = async () => {
+    const restaurantInfoSnapshot = await getDoc(doc(db, "restaurant", "info"));
+    setRestaurantInfo(restaurantInfoSnapshot.data() as IRestraunt);
+  };
+
+  const fetchData = async () => {
+    await fetchRestaurantInfo();
+    setIsReady(true);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <DataProviderContext.Provider value={{}}>
+    <DataProviderContext.Provider value={{ restaurantInfo }}>
       {isReady ? (
         children
       ) : (
--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -1,9 +1,16 @@
 import { Box } from "@chakra-ui/react";
 
+import { useDataProvider } from "../components/data-provider";
+
 export const Info = () => {
+  const { restaurantInfo } = useDataProvider();
+
+  if (!restaurantInfo) return null;
+
   return (
     <Box>
       <p>Info page</p>
+      <p>Restaurant Name {restaurantInfo.name}</p>
     </Box>
   );
 };
