7ce400f1c1100bb317159d6c014105844eca67c5
Azim Ahmed <azim.ahmed7@gmail.com>
2023-05-19 11:34:03 -0400

    Skeleton accordion with comment form

--- src/pages/checkout.tsx
+++ src/pages/checkout.tsx
@@ -1,9 +1,53 @@
-import { Box } from "@chakra-ui/react";
+import {
+  Accordion,
+  AccordionButton,
+  AccordionItem,
+  AccordionPanel,
+  Box,
+  FormControl,
+  FormLabel,
+  Textarea,
+  VStack,
+} from "@chakra-ui/react";
+import { useForm } from "react-hook-form";
+import { IOrder } from "../models";
+import { BottomButton } from "../components/bottom-button";
+import { useDataProvider } from "../components/data-provider";
+import { calculateOrderTotal } from "../utils/calculations";
 
 export const Checkout = () => {
+  const { lines } = useDataProvider();
+  const { register, handleSubmit } = useForm<IOrder>();
+
+  const onSubmit = (data: IOrder) => console.log(data);
+
   return (
-    <Box>
-      <p>checkout page</p>
-    </Box>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Accordion defaultIndex={[0, 1, 2]} allowMultiple>
+        <AccordionItem>
+          <AccordionButton bg="gray.200">CONTACT</AccordionButton>
+          <AccordionPanel>Something</AccordionPanel>
+        </AccordionItem>
+        <AccordionItem>
+          <AccordionButton bg="gray.200">PAYMENT METHOD</AccordionButton>
+          <AccordionPanel>Something</AccordionPanel>
+        </AccordionItem>
+        <AccordionItem>
+          <AccordionButton bg="gray.200">COMMENTS</AccordionButton>
+          <AccordionPanel>
+            <VStack mt={4}>
+              <FormControl>
+                <FormLabel>Comments</FormLabel>
+                <Textarea placeholder="Comments..." {...register("comments")} />
+              </FormControl>
+            </VStack>
+          </AccordionPanel>
+        </AccordionItem>
+      </Accordion>
+      <BottomButton
+        label="Place pick up order"
+        total={calculateOrderTotal(lines, 13).toFixed(2)}
+      />
+    </form>
   );
 };
